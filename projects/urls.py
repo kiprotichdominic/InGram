00b73ce00ac106f0e2e11274beb00ae82caa5f1e
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-16 19:59:48 +0300

    fix api functionality

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ProjectList, ProjectDetail
 
 urlpatterns = [
-    path('<int:pk>', ProjectDetail.as_view()),
-    path('project/', ProjectList.as_view()),
+    path('/project/<int:pk>', ProjectDetail.as_view()),
+    path('/project/', ProjectList.as_view()),
 ]
