e5baed070e30cebfff3d9cda0076f167a51862a3
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-16 20:29:23 +0300

    add a read only for none authors

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import ProjectSerializer
 
 
@@ -7,5 +8,6 @@ class ProjectList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = ProjectSerializer
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,) 
     queryset = Post.objects.all()
     serializer_class = ProjectSerializer
