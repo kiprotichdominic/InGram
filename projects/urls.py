29cf5cd2bf52e016b1ce1d11e69d7093c27cd9d6
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-16 18:39:13 +0300

    add url pattenrs for my api

--- projects/urls.py
+++ projects/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+
+from .views import ProjectList, ProjectDetail
+
+urlpatterns = [
+    path('<int:pk>', ProjectDetail.as_view()),
+    path('project/', ProjectList.as_view()),
+]
