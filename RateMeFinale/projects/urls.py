325408917dfbf769a2b2c405704da13a1209fc99
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-17 08:15:55 +0300

    fix commit

--- RateMeFinale/projects/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ProjectList, ProjectDetail
-
-urlpatterns = [
-    path('project/<int:pk>', ProjectDetail.as_view()),
-    path('project/', ProjectList.as_view()),
-]
