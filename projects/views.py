44f083616ca9d682229a238c68f2d6e832cc1d0d
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-17 08:31:27 +0300

    change django version

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, UserSerializer
 
 
 class ProjectList(generics.ListCreateAPIView):
