2d77b88559fd22cf1fb593cd3597283e4ca33233
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-17 09:27:27 +0300

    create users endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProjectList, ProjectDetail
+from .views import ProjectList, ProjectDetail, UserDetail, UserList
 
 urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()), 
     path('project/<int:pk>', ProjectDetail.as_view()),
     path('project/', ProjectList.as_view()),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model 
 from rest_framework import generics
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
@@ -7,7 +8,16 @@ from .serializers import ProjectSerializer, UserSerializer
 class ProjectList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = ProjectSerializer
+    
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,) 
     queryset = Post.objects.all()
     serializer_class = ProjectSerializer
+    
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
