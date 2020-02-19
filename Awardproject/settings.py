98f5c2ed81210301d5051f556c4a9cfbaeeda376
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-19 15:46:59 +0300

    fix landing page

--- Awardproject/settings.py
+++ Awardproject/settings.py
@@ -148,7 +148,7 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES': [
-'rest_framework.permissions.IsAuthenticated',
+'rest_framework.permissions.IsAdminUser',
 ],
 'DEFAULT_AUTHENTICATION_CLASSES': [ 
 'rest_framework.authentication.SessionAuthentication',
