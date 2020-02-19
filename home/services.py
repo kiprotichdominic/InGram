8c4e437337631d2e297a43504e8a51a98a0c1471
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-02-19 16:56:32 +0300

    fix app

--- home/services.py
+++ home/services.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def get_posts():
-    url = 'https://blogapi001.herokuapp.com/api/v1//api/v1/?format=json'
+    url = 'https://blogapi001.herokuapp.com/api/v1/?format=json'
     r = requests.get(url)
     posts= r.json()
     return posts                                                                                                                                                                                                                                                                                                                                                                                                                                            
